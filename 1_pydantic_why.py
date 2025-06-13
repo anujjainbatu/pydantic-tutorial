ffd8d6d4ab56b2598432e76c505b5cc312e33d2e
Anuj Jain <anujjainbatu@gmail.com>
2025-06-13 21:16:48 +0530

    data validation

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,8 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
-    name: str
-    age: int
+    name: Annotated[str, Field(max_length=50, title="Patient Name", description="Full name of the patient", example="John Doe")]
+    email: EmailStr
+    linked_in: AnyUrl
+    age: int = Field(gt=0, lt=120)
+    weight: Annotated[float, Field(gt=0, strict=True, description="Weight in kilograms", example=70.5)]
+    married: Annotated[Optional[bool], False, Field(default=None, description="Is the patient married?")]
+    allergies: Annotated[Optional[List[str]], Field(default=None, description="List of patient allergies")]
+    contact_details: Dict[str, str]
 '''
 def insert_patient_data(name: str, age: int):
     if type(name) == str and type(age) == int:
@@ -23,7 +30,8 @@ def insert_patient_data(patient: Patient):
     print(patient.age)
     print("Patient data inserted successfully.")
 
-patient_info = {"name": "John Doe", "age": 35}
+patient_info = {"name": "John Doe", "email": "abc@gmail.com", "linked_in": "http://linkedin.com/in/abc", "age": 35, "weight": 70.5,
+                "married": True, "allergies": ["pollen", "nuts"], "contact_details": {"email": "abc@gmail.com", "phone": "1234567890"}}
 
 patient1 = Patient(**patient_info)
 
