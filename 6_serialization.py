3e9319014e2be791f4c7e4390921251c25bc01dd
Anuj Jain <anujjainbatu@gmail.com>
2025-06-15 11:38:27 +0530

    exporting pydantic model

---
+++ 6_serialization.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+class Patient(BaseModel):
+
+    name: str
+    gender: str = 'Male'
+    age: int
+    address: Address
+
+address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {'name': 'nitish', 'age': 35, 'address': address1}
+
+patient1 = Patient(**patient_dict)
+
+temp = patient1.model_dump(include={'name', 'address'}, exclude={'address':['state']}, exclude_unset=True) #exclude_unset=True will not include fields that were not set in the model
+
+print(temp)
+print(type(temp))
